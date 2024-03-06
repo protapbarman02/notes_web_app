6bfe9e9f2b8d119da7f4b2e97106c7b5d534a5e7
protapbarman02 <159292490+protapbarman02@users.noreply.github.com>
2024-03-07 00:26:43 +0530

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,37 @@
+<?php
+require("includes/dbconnection.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/navbar.css">
+    <link rel="stylesheet" href="styles/main.css">
+    <link rel="stylesheet" href="styles/footer.css">
+    <link rel="stylesheet" href="styles/myaccount.css">
+    <link rel="stylesheet" href="//cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+    <nav>
+        <div>
+            <a href="index.php">
+                <img src="images/logo.png" alt="" class="logo">
+            </a>
+        </div>
+        <div>
+            My Notes
+        </div>
+        <div>
+            <a href="myaccount.php">
+                <img src="images/account.png" alt="" class="acc">
+            </a>
+        </div>
+    </nav>
